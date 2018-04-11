235b8d6e97a665bad51d819ee89bcb89daedf664
Shaq <shaquillenhall@yahoo.com>
2018-04-11 17:22:25 -0400

    Add files via upload
    commit of site pages

---
+++ home.php
@@ -0,0 +1,13 @@
+<div class="page" title="Page 1">
+  <div class="layoutArea">
+    <div class="column">
+      <img class="alignnone size-medium wp-image-41 evolve-img" src="http://localhost:6969/evlos-demo/wp-content/uploads/evolve.png" alt="" width="300" height="40" />
+      <p class="home-paragraph" style="text-align: center;">
+        We are passionately engaged in work worth doing. Our experienced and talented professionals listen to customers and work closely with them to develop innovative solutions to difficult problems.
+      </p>
+      <p class="home-paragraph" style="text-align: center;">
+        We bring efficiency and effectiveness to every project using tools and techniques developed and refined through years of commercial and private sector experience. Helping each customer achieve their organization’s vision is what drives us. Evlos strives to maximize the value of services and products provided to it steam members.
+      </p>
+    </div>
+  </div>
+</div>
---
+++ solve-with-us.php
@@ -0,0 +1,78 @@
+<div class="panel-content" id="solve-with-us-intro">
+	<p class="solve-with-us-header">
+		Join The Evlos Team
+	</p>
+	<p class="solve-with-us-top-body">
+		Evlos Technology is looking for problem solvers and "opportunity capitalizers." These individuals are able to understand how to use their background to solve customer problems and innovate in a manner that brings new opportunities to light.
+	</p>
+</div>
+<div class="panel-content job-left-section" id="systems-engineer-section">
+	<div uk-grid>
+		<div class="uk-width-1-2">
+			<div class="job-section">
+				<p class="job-title">
+					Systems Engineer
+				</p>
+				<p class="job-description">
+					Evlos Technology is looking for problem solvers and "opportunity capitalizers." These individuals are able to understand how to use their background to solve customer problems and innovate in a manner that brings new opportunities to light.
+				</p>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="panel-content job-right-section" id="network-engineer-section">
+	<div uk-grid>
+		<div class="uk-width-1-2 uk-push-1-2">
+			<div class="job-section">
+				<p class="job-title">
+					Network Engineer
+				</p>
+				<p class="job-description">
+					Evlos Technology is looking for problem solvers and "opportunity capitalizers." These individuals are able to understand how to use their background to solve customer problems and innovate in a manner that brings new opportunities to light.
+				</p>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="panel-content job-left-section" id="software-engineer-section">
+	<div uk-grid>
+		<div class="uk-width-1-2">
+			<div class="job-section">
+				<p class="job-title">
+					Software Engineer
+				</p>
+				<p class="job-description">
+					Evlos Technology is looking for problem solvers and "opportunity capitalizers." These individuals are able to understand how to use their background to solve customer problems and innovate in a manner that brings new opportunities to light.
+				</p>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="panel-content job-right-section" id="project-manager-section">
+	<div uk-grid>
+		<div class="uk-width-1-2 uk-push-1-2">
+			<div class="job-section">
+				<p class="job-title">
+					Project Manager
+				</p>
+				<p class="job-description">
+					Evlos Technology is looking for problem solvers and "opportunity capitalizers." These individuals are able to understand how to use their background to solve customer problems and innovate in a manner that brings new opportunities to light.
+				</p>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="panel-content job-left-section" id="web-developer-section">
+	<div uk-grid>
+		<div class="uk-width-1-2">
+			<div class="job-section">
+				<p class="job-title">
+					Web Developer
+				</p>
+				<p class="job-description">
+					Evlos Technology is looking for problem solvers and "opportunity capitalizers." These individuals are able to understand how to use their background to solve customer problems and innovate in a manner that brings new opportunities to light.
+				</p>
+			</div>
+		</div>
+	</div>
+</div>
